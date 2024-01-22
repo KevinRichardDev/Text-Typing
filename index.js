7357aa1350d6d5907c6d144229303f12ad63921a
KevinRichardDev <kevin.richard0@orange.fr>
2024-01-22 05:26:12 +0100

    loop()

--- index.js
+++ index.js
@@ -2,14 +2,23 @@ const target = document.getElementById("target");
 
 let array = ["développeur", "photographe", "créatif"];
 let wordIndex = 0;
+let letterIndex = 0;
 
 const createLetter = () => {
   const letter = document.createElement("span");
   target.appendChild(letter);
 
-  letter.textContent = array [wordIndex][0];
+  letter.textContent = array [wordIndex][letterIndex];
 }
 
-createLetter();
+const loop = () => {
+  setTimeout(() => {
+    if(letterIndex < array[wordIndex].length) {
+      createLetter();
+      letterIndex++;
+      loop();
+    }
+  }, 80);
+}
 
-setInterval(createLetter, 2000);
+loop ();
